89d7e4005e0c353934d9372c903ffd7cd6b855e1
Albert <Albert_Jyllhed@hotmail.com>
2026-02-04 00:19:59 +0100

    Added some nice formatting to data printouts.

--- ADOManager.cs
+++ ADOManager.cs
@@ -1,15 +1,70 @@
-﻿namespace Database_Project_SchoolDB
+﻿using Microsoft.Data.SqlClient;
+
+namespace Database_Project_SchoolDB
 {
     internal static class ADOManager
     {
+        private static readonly string _connectionString = "Server = localhost;" +
+            "Database = SchoolDB;" +
+            "Integrated Security = True;" +
+            "Trust Server Certificate = True;";
+
         internal static void GetEmployees()
         {
-            Console.WriteLine("Getting Employees...");
+            string query = "SELECT * FROM Employees";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (var reader = new SqlCommand(query, connection).ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine($"{reader.GetName(i)}: {reader.GetValue(i)}");
+                            }
+                            Console.WriteLine("----------------------------------------");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                connection.Close();
+            }
         }
 
         internal static void AddNewEmployee()
         {
-            Console.WriteLine("Adding New Employee...");
+            string query = "INSERT INTO Employees (FirstName, LastName, Salary, EmployeeTypeId, DepartmentId) " +
+                "VALUES (@FirstName, @LastName, @Email, @Age)";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand(query, connection))
+                {
+                    //command.Parameters.AddWithValue("@FirstName", firstName);
+
+                    try
+                    {
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+                        Console.WriteLine($"{rowsAffected} rows changed.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    connection.Close();
+                }
+            }
         }
 
         internal static void GetStudentGrades()
---
+++ Utils.cs
@@ -0,0 +1,18 @@
+﻿namespace Database_Project_SchoolDB
+{
+    public static class Utils
+    {
+        public static string CenterString(string s, int width)
+        {
+            if (s.Length >= width)
+            {
+                return s;
+            }
+
+            int leftPadding = (width - s.Length) / 2;
+            int rightPadding = (width - s.Length - leftPadding);
+
+            return new string(' ', leftPadding) + s + new string(' ', rightPadding);
+        }
+    }
+}
